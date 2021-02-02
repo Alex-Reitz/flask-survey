d291517318d9aa6640c405a7eee5db6e83015210
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-02-02 16:11:32 -0500

    Initial routes setup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys
+from surveys import personality_quiz, satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
@@ -11,10 +11,18 @@ responses = []
 
 @app.route("/")
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html", personality_Title=personality_quiz.title, personality_Intructions=personality_quiz.instructions, satisfaction_Title=satisfaction_survey.title, satisfaction_Instructions=satisfaction_survey.instructions)
 
 @app.route('/personality')
-def show_survey(survey):
-    personality = surveys.personality_quiz
-    return render_template("personality.html", personality=personality)
+def show_survey():
+    return render_template("personality.html")
     
+@app.route('/satisfaction')
+def satisfaction_text():
+    return render_template("satisfaction.html")
+
+#Route for satisfaction questions
+@app.route("/satisfaction/<question>/0")
+def satisfactions_questions(question):
+    print(satisfaction_survey.questions)
+    return render_template("satisfaction_questions.html", question=satisfaction_survey.questions)
