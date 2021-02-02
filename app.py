f4ae2ed699747c65232660a3577e9416d9caec21
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-02-02 13:50:11 -0500

    update

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import Question, Survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route('/user/<survey>')
+def show_survey(survey):
+    print(surveys)
+    name = surveys
+    survey = surveys[name]
+    return f"{{survey}}"
+    
--- surveys.py
+++ surveys.py
@@ -1,3 +1,4 @@
+
 class Question:
     """Question on a questionnaire."""
 
