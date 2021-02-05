e0db5ea9e2faf6d5e86e49cbac4b0e2d1b225ce4
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-02-05 09:21:18 -0500

    routes complete

--- app.py
+++ app.py
@@ -1,42 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import personality_quiz, satisfaction_survey
+from surveys import satisfaction_survey as survey
 
-
+RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 responses = []
 
 @app.route("/")
-def home_page():
-    return render_template("home.html", personality_Title=personality_quiz.title, personality_Intructions=personality_quiz.instructions, 
-    satisfaction_Title=satisfaction_survey.title, satisfaction_Instructions=satisfaction_survey.instructions)
-
-@app.route('/personality')
-def show_survey():
-    return render_template("personality.html", personality_Title=personality_quiz.title)
-    
-@app.route('/satisfaction')
-def satisfaction_text():
-    return render_template("satisfaction.html", satisfaction_Title=satisfaction_survey.title)
-
-#Route for satisfaction questions
-@app.route("/satisfaction_questions/question/<int:index>")
-def satisfactions_questions(index):
-    question = satisfaction_survey.questions[index].question.self
-    return render_template("satisfaction_questions.html", question=question)
+def show_survey_start():
+    return render_template("home.html", survey=survey)
 
 
-@app.route("/add-response", methods=["POST"])
-def add_response():
-    response = request.form("name")
-    responses.append(response)
-    index = responses.length
+@app.route("/begin", methods=["POST"])
+def start_survey():
+
+    return redirect("/questions/0")
 
 
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    choice = request.form['answer']
+    responses.append(choice)
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
 
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    if (responses is None):
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "question.html", question_num=qid, question=question)
+
+
+@app.route("/complete")
+def complete():
+
+    return render_template("thank_you.html")
